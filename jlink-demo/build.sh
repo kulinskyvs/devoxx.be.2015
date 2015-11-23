e4bf28a3c6a9c5b2256021b56f2d7d6a5381f491
Vadim Kulinsky <vkulinsky@kyriba.com>
2015-11-23 10:33:11 +0100

    jlink example is updated

--- jlink-demo/build.sh
+++ jlink-demo/build.sh
@@ -2,7 +2,8 @@
 rm -rf hello.world.app
 mkdir hello.world.app
 
-$JAVA9_HOME/bin/jlink --modulepath $JAVA9_HOME/jmods:../sample2/mlib --addmods hello.world --output hello.world.app
+#$JAVA9_HOME/bin/jlink --modulepath $JAVA9_HOME/jmods:../sample2/mlib --addmods hello.world --output hello.world.app
+$JAVA9_HOME/bin/jlink --modulepath "$JAVA9_HOME/jmods;../sample2/mlib" --addmods hello.world --output hello.world.app
 
 du -sh $JAVA9_HOME
 du -sh hello.world.app
