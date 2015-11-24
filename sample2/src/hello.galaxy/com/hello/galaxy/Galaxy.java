4e42043cdc41ead3694760b8985972443c1f248f
Vadim Kulinsky <vkulinsky@kyriba.com>
2015-11-24 23:13:22 +0100

    Sample 2 is updated

--- sample2/src/hello.galaxy/com/hello/galaxy/Galaxy.java
+++ sample2/src/hello.galaxy/com/hello/galaxy/Galaxy.java
@@ -2,7 +2,7 @@ package com.hello.galaxy;
 
 public class Galaxy {
 
-	public static final String GALAXY_NAME = "Milky Way";
+	private static final String GALAXY_NAME = "Milky Way";
 
     public String getName() {
     	return GALAXY_NAME;
